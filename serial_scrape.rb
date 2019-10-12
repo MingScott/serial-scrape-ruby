e2ce725f4d5c2682f0f8c9e4c63c8a150b6a02a9
Ming Scott <ming.scott@gmail.com>
2019-10-12 02:15:15 -0300

    Built main book-building loop. Wrote framework for writing custom classes and using them in the pipeline. The calls are example code, will be amended eventually

--- serial_scrape.rb
+++ serial_scrape.rb
@@ -1,17 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
+require 'optparse'
 
-@title = ""
-@toc = ""
-@text = ""
+#Option parser
+@options = Hash.new
+OptionParser.new do |o|
+	o.on("-t", "--title", "Provide title"){ |t| @options["title"] = t }
+	o.on("-l", "--link" , "Provide 1st chapter link"){ |l| @options["start"] = l }
+end.parse!
 
+#Generic chapter reading class
 class Chapter
 	def initialize(url)
-		@doc = Nokogiri::HTML open url
-		@url = url
+		@doc = 		Nokogiri::HTML open url
+		@url = 		url
 	end
+
 	def title ;		@doc.css("h1").first.content ; end
-	def text ; 		@doc.css("p"); end
+	def text ; 		@doc.css("p").to_s; end
+	def url ;		@url; end
+
 	def linksearch(pattern)
 		find = false
 		links = @doc.css "a"
@@ -20,25 +28,77 @@ class Chapter
 				find = l["href"]
 			end
 		end
-		unless find[0..3] == "http"
-			domain_index = @url.index("/",8)
-			find = @url[0..domain_index-1] + find
+		if find
+			unless find[0..3] == "http"
+				domain_index = @url.index("/",8)
+				find = @url[0..domain_index-1] + find
+			end
 		end
 		return find
 	end
-	def nextch;		self.linksearch("NEXT"); end
-	def prevch;		self.linksearch("PREV"); end
+
+	def nextch;		self.linksearch "NEXT"
+	end
+	def prevch;		self.linksearch "PREV" 
+	end
+end 
+
+class RRChapter < Chapter
+	def text
+		foreword = @doc.css("div.author-note")
+		doc = @doc.css("div.chapter-inner.chapter-content").first
+		doc = doc.css "p"
+		return "<div align=\"right\"><i>#{foreword.to_s}</i></div>\n#{doc.to_s}\n"
+	end #Chapter class customized for royalroad
 end
 
-def Book(title, first_chapter_url)
-
-
-url = 'https://www.royalroad.com/fiction/25225/delve/chapter/410382/50-baggage'
-url = 'https://www.parahumans.net/2019/09/14/from-within-16-10/'
-ch = Chapter.new(url)
-puts ch.prevch
-puts ch.nextch
-puts ch.title
-File.open 'misc.html', 'w' do |f|
-	f.puts Chapter.new(url).text
+class Book
+	def initialize(chap, title="Beginning")
+		@next_url = chap.nextch
+		@title = title
+		@chap = chap
+		@body = ""
+		@toc = "<h1>Table of Contents</h1>"
+		@ind = 1
+		until @next_url == false
+			@next_url = @chap.nextch
+			@body << "<h1 id=\"chapter#{@ind.to_s}\">#{@chap.title}</h1>\n"
+			@body << @chap.text + "\n"
+			@toc  << "<a href=\"#chapter#{@ind}\">#{@chap.title}</a><br>\n"
+			@ind  += 1
+			if @next_url
+				@chap = @chap.class.new @next_url
+			end
+		end
+	end
+	def full_text
+		title = "<h1>#{@title}</h1>\n"
+		return title + @toc + @body
+	end
+	def shelve(fname);
+		File.open fname, 'w' do |f| ; f.puts self.full_text;
+		end
+	end 
 end
+
+def classFinder(url) #if you add custom classes, add the pattern to search for to verify them here
+	patterns = {
+		"royalroad" => RRChapter
+	}
+	@chapclass = ""
+	patterns.keys.each do |k|
+		@chapclass = if url.include? k
+			patterns[k]
+		end
+	end
+	if @chapclass == ""
+		@chapclass = Chapter
+	end
+	return @chapclass
+end
+
+url = "https://www.royalroad.com/fiction/25225/delve/chapter/368012/1-woodland"
+
+ch1 = classFinder(url).new url
+delve = Book.new ch1, "Delve"
+delve = delve.shelve "delve.html"
